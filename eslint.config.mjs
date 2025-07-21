27c44d8090860f3622f02b577ca00f719efa6236
Yãry Castilho <andradeya4@gmail.com>
2025-07-21 14:27:43 -0300

    Adicionando midlleware para capturar erros

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig([
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
   { rules: {
-     'no-unused-vars': 'off',
+     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
 }},
 ]);
 
