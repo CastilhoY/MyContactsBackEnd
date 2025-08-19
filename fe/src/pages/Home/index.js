296d4faa796e61509b4336759f0b1b17df203ee6
Yãry Castilho <andradeya4@gmail.com>
2025-08-19 15:11:30 -0300

    Refatorando o Header

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,3 +1,87 @@
+import { Container, Header, ListContainer, Card, InputSearchContainer } from "./styles";
+
+import arrow from '../../assets/images/icons/arrow.svg'
+import trash from '../../assets/images/icons/trash.svg'
+import edit from '../../assets/images/icons/edit.svg'
+
 export default function Home(){
-    return 'homepage'
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..."/>
+      </InputSearchContainer>
+
+      <Header>
+        <strong> 3 Contatos </strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+            <ListContainer>
+                <header>
+                    <button type="button">
+                        <span>Nome</span>
+                        <img src={arrow} alt="Arrow" />
+                    </button>
+                </header>
+
+                <Card>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>Yãry Castilho</strong>
+                            <small>instagram</small>
+                        </div>
+                        <span>andradeya4@gmail.com</span>
+                        <span>(19) 99999-9999</span>
+                    </div>
+
+                    <div className="actions">
+                        <a href="/">
+                          <img src={edit} alt="Edit" />
+                        </a>
+                        <button type="button">
+                            <img src={trash} alt="Delete" />
+                        </button>
+                    </div>
+                </Card>
+                                <Card>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>Yãry Castilho</strong>
+                            <small>instagram</small>
+                        </div>
+                        <span>andradeya4@gmail.com</span>
+                        <span>(19) 99999-9999</span>
+                    </div>
+
+                    <div className="actions">
+                        <a href="/">
+                          <img src={edit} alt="Edit" />
+                        </a>
+                        <button type="button">
+                            <img src={trash} alt="Delete" />
+                        </button>
+                    </div>
+                </Card>
+                                <Card>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>Yãry Castilho</strong>
+                            <small>instagram</small>
+                        </div>
+                        <span>andradeya4@gmail.com</span>
+                        <span>(19) 99999-9999</span>
+                    </div>
+
+                    <div className="actions">
+                        <a href="/">
+                          <img src={edit} alt="Edit" />
+                        </a>
+                        <button type="button">
+                            <img src={trash} alt="Delete" />
+                        </button>
+                    </div>
+                </Card>
+            </ListContainer>
+    </Container>
+  )
 }
