ac631a27982b08913e4baecc67f02b73c966ec1b
Yãry Castilho <andradeya4@gmail.com>
2025-08-19 14:39:26 -0300

    Criação das rotas

--- fe/src/components/App/index.js
+++ fe/src/components/App/index.js
@@ -1,21 +1,27 @@
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from '../../assets/styles/global'
-import defaultTheme from '../../assets/styles/themes/default'
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
 
-import Header from '../Header'
+import Header from '../Header';
+import Routes from '../../Routes';
+import ContactsList from '../ContactsList';
 
 import { Container } from './styles';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-        <GlobalStyles />
+    <BrowserRouter>
+        <ThemeProvider theme={defaultTheme}>
+            <GlobalStyles />
 
-        <Container>
-            <Header />
-        </Container>
+            <Container>
+                <Header />
+                <Routes />
+            </Container>
 
-    </ThemeProvider>
+        </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
