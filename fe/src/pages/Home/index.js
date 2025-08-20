ae9c7bfbe3203f023d30627ce876594056f8bf6a
Yãry Castilho <andradeya4@gmail.com>
2025-08-20 10:57:55 -0300

    Adição de input, submit e button

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import { Container, Header, ListContainer, Card, InputSearchContainer } from "./styles";
+import { Link } from 'react-router-dom'
 
 import arrow from '../../assets/images/icons/arrow.svg'
 import trash from '../../assets/images/icons/trash.svg'
@@ -13,7 +14,7 @@ export default function Home(){
 
       <Header>
         <strong> 3 Contatos </strong>
-        <a href="/">Novo Contato</a>
+        <Link to="/new">Novo Contato</Link>
       </Header>
 
             <ListContainer>
@@ -35,47 +36,9 @@ export default function Home(){
                     </div>
 
                     <div className="actions">
-                        <a href="/">
+                        <Link to="/edit/123">
                           <img src={edit} alt="Edit" />
-                        </a>
-                        <button type="button">
-                            <img src={trash} alt="Delete" />
-                        </button>
-                    </div>
-                </Card>
-                                <Card>
-                    <div className="info">
-                        <div className="contact-name">
-                            <strong>Yãry Castilho</strong>
-                            <small>instagram</small>
-                        </div>
-                        <span>andradeya4@gmail.com</span>
-                        <span>(19) 99999-9999</span>
-                    </div>
-
-                    <div className="actions">
-                        <a href="/">
-                          <img src={edit} alt="Edit" />
-                        </a>
-                        <button type="button">
-                            <img src={trash} alt="Delete" />
-                        </button>
-                    </div>
-                </Card>
-                                <Card>
-                    <div className="info">
-                        <div className="contact-name">
-                            <strong>Yãry Castilho</strong>
-                            <small>instagram</small>
-                        </div>
-                        <span>andradeya4@gmail.com</span>
-                        <span>(19) 99999-9999</span>
-                    </div>
-
-                    <div className="actions">
-                        <a href="/">
-                          <img src={edit} alt="Edit" />
-                        </a>
+                        </Link>
                         <button type="button">
                             <img src={trash} alt="Delete" />
                         </button>
