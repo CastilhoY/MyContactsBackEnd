bdbe54a16a64d8cb091653a149ad6be77edde974
Yãry Castilho <andradeya4@gmail.com>
2025-08-20 12:28:49 -0300

    Criando Modal

--- fe/src/components/ContactForm/styles.js
+++ fe/src/components/ContactForm/styles.js
@@ -5,5 +5,9 @@ export const Form = styled.form`
 `;
 
 export const ButtonContainer = styled.div`
-margin-top: 24px;
+  margin-top: 24px;
+
+  button {
+    width: 100%;
+  }
 `;
--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom'
 import arrow from '../../assets/images/icons/arrow.svg'
 import trash from '../../assets/images/icons/trash.svg'
 import edit from '../../assets/images/icons/edit.svg'
+import Modal from "../../components/Modal";
 
 export default function Home(){
   return (
     <Container>
+      <Modal danger />
+
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato..."/>
+        <input type="text" placeholder="Pesquise pelo nome..."/>
       </InputSearchContainer>
 
       <Header>
@@ -17,34 +20,33 @@ export default function Home(){
         <Link to="/new">Novo Contato</Link>
       </Header>
 
-            <ListContainer>
-                <header>
-                    <button type="button">
-                        <span>Nome</span>
-                        <img src={arrow} alt="Arrow" />
-                    </button>
-                </header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
 
-                <Card>
-                    <div className="info">
-                        <div className="contact-name">
-                            <strong>Yãry Castilho</strong>
-                            <small>instagram</small>
-                        </div>
-                        <span>andradeya4@gmail.com</span>
-                        <span>(19) 99999-9999</span>
-                    </div>
-
-                    <div className="actions">
-                        <Link to="/edit/123">
-                          <img src={edit} alt="Edit" />
-                        </Link>
-                        <button type="button">
-                            <img src={trash} alt="Delete" />
-                        </button>
-                    </div>
-                </Card>
-            </ListContainer>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Yãry Castilho</strong>
+            <small>instagram</small>
+          </div>
+          <span>andradeya4@gmail.com</span>
+          <span>(19) 99999-9999</span>
+        </div>
+        <div className="actions">
+          <Link to="/edit/123">
+            <img src={edit} alt="Edit" />
+          </Link>
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+      </ListContainer>
     </Container>
   )
 }
